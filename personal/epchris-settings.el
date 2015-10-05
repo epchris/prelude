67b3d1d04f03bc24fdd4c92ba1bb66520f136d00
Christopher TenHarmsel <ctenharmsel@groupon.com>
2015-10-05 16:11:08 -0500

    Not sure why this needs to be removed, but it works

--- personal/epchris-settings.el
+++ personal/epchris-settings.el
@@ -231,7 +231,7 @@
   (interactive)
   (kill-buffer)
   (jump-to-register :magit-fullscreen))
-(define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
+;; (define-key magit-status-mode-map (kbd "q") 'magit-quit-session)
 
 ;;; Origami Settings
 (require 'origami)
